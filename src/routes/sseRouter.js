923d1491f9ddd13b4a0237fdfb0cd563f13afff7
jeffcaradona <jeffcaradona@gmail.com>
2025-10-03 17:01:59 -0500

    removed route without a middleware function

--- src/routes/sseRouter.js
+++ src/routes/sseRouter.js
@@ -5,7 +5,7 @@ import * as sseSendTimeController from "../controllers/sseSendTimeController.js"
 import * as ssePrivateSessionController from "../controllers/ssePrivateSessionController.js";
 // SSE endpoint for server time
 router.get("/time", sseSendTimeController.time);
-router.get("/privatesession",null)
+//router.get("/privatesession",null)
 
 
 export default router;
